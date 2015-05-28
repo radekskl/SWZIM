4fb4431aa3993fdc0675cd98171d3628db492859
michaju <michal.sobczak@student.wat.edu.pl>
2015-05-28 11:07:03 +0200

    layers for user

--- SWZIM-WEBWeb/Helpers/LayersHelper.cs
+++ SWZIM-WEBWeb/Helpers/LayersHelper.cs
@@ -7,11 +7,11 @@ namespace SWZIM_WEBWeb.Helpers
 {
     public class LayersHelper
     {
-        public static List<Layers> GetLayersList()
+        public static List<Layers> GetLayersList(int userId)
         {
             using (var db = new SWZIM_dbEntities())
             {
-                return db.Layers.ToList();
+                return db.Layers.Where(l=> l.Groups.All(g=> g.Users.Where(u => u.ID == userId).Count() > 0)).ToList();
             }
         }
     }

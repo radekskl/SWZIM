9695541f7a9d15c113155757f802d78fe11ce6f3
michaju <michal.sobczak@student.wat.edu.pl>
2015-05-28 12:27:19 +0200

    json dla wszystkich elementow

--- SWZIM-WEBWeb/Controllers/MapController.cs
+++ SWZIM-WEBWeb/Controllers/MapController.cs
@@ -8,6 +8,8 @@ namespace SWZIM_WEBWeb.Controllers
 {
     public class MapController : Controller
     {
+
+        private SWZIM_dbEntities db = new SWZIM_dbEntities();
         // GET: Map
         [SharePointContextFilter]
         public ActionResult Index()
@@ -24,6 +26,18 @@ namespace SWZIM_WEBWeb.Controllers
             }
             else
                 return RedirectToAction("Error", "Home");
+        }
+
+        public JsonResult GetAllElementsForLayout(int id)
+        {
+            return Json(db.LayoutElements.Where(le => le.LayersId == id).ToList());
+        }
+
+        public JsonResult GetAllEventsForLayout(int id)
+        {
+            var groups = db.Groups.Where(g => g.Layers.Any(l => l.Id == id));
+            // do poprawy
+            return Json(groups);
         }
 
     }

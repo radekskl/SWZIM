4cd8ddb9b3eb188349b5167f89a1f6186743463b
michaju <michal.sobczak@itawat.onmicrosoft.com>
2015-05-28 09:10:38 +0200

    warstwy z bazy do menu

---
+++ SWZIM-WEBWeb/Helpers/LayersHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SWZIM_WEBWeb.Helpers
+{
+    public class LayersHelper
+    {
+        public static List<Layers> GetLayersList()
+        {
+            using (var db = new SWZIM_dbEntities())
+            {
+                return db.Layers.ToList();
+            }
+        }
+    }
+}

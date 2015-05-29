8c97654a7748e9376479aeea47b6064043641677
michaju <michal.sobczak@student.wat.edu.pl>
2015-05-29 14:27:51 +0200

    dodany event helper

---
+++ SWZIM-WEBWeb/Helpers/EventsHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SWZIM_WEBWeb.Helpers
+{
+    public class EventsHelper
+    {
+        public static List<Events> GetLatestEventsForUser(string userId, int limit)
+        {
+            using (var db = new SWZIM_dbEntities())
+            {
+                var userGroup = db.Users.Find(userId).GroupId;
+                return db.Events.Where(l => l.Groups.Any(g => g.Id == userGroup)).OrderByDescending(x=> x.CreatedAt).Take(limit).ToList();
+            }
+        }
+    }
+}

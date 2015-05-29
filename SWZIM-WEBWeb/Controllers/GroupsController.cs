d689740355606746f38732d387f754a46fe02427
michaju <michal.sobczak@student.wat.edu.pl>
2015-05-29 19:24:14 +0200

    kontroler dla grup

---
+++ SWZIM-WEBWeb/Controllers/GroupsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SWZIM_WEBWeb;
+
+namespace SWZIM_WEBWeb.Controllers
+{
+    public class GroupsController : Controller
+    {
+        private SWZIM_dbEntities db = new SWZIM_dbEntities();
+
+        // GET: Groups
+        [SharePointContextFilter]
+        public ActionResult Index()
+        {
+            return View(db.Groups.ToList());
+        }
+
+        // GET: Groups/Details/5
+        [SharePointContextFilter]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Groups groups = db.Groups.Find(id);
+            if (groups == null)
+            {
+                return HttpNotFound();
+            }
+            return View(groups);
+        }
+
+        // GET: Groups/Create
+        [SharePointContextFilter]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Groups/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [SharePointContextFilter]
+        public ActionResult Create([Bind(Include = "Id,Name")] Groups groups)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Groups.Add(groups);
+                db.SaveChanges();
+                return RedirectToAction("Index", new { SPHostUrl = SharePointContext.GetSPHostUrl(HttpContext.Request).AbsoluteUri });
+            }
+
+            return View(groups);
+        }
+
+        // GET: Groups/Edit/5
+        [SharePointContextFilter]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Groups groups = db.Groups.Find(id);
+            if (groups == null)
+            {
+                return HttpNotFound();
+            }
+            return View(groups);
+        }
+
+        // POST: Groups/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [SharePointContextFilter]
+        public ActionResult Edit([Bind(Include = "Id,Name")] Groups groups)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(groups).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index", new { SPHostUrl = SharePointContext.GetSPHostUrl(HttpContext.Request).AbsoluteUri });
+            }
+            return View(groups);
+        }
+
+        // GET: Groups/Delete/5
+        [SharePointContextFilter]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Groups groups = db.Groups.Find(id);
+            if (groups == null)
+            {
+                return HttpNotFound();
+            }
+            return View(groups);
+        }
+
+        // POST: Groups/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [SharePointContextFilter]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Groups groups = db.Groups.Find(id);
+            db.Groups.Remove(groups);
+            db.SaveChanges();
+            return RedirectToAction("Index", new { SPHostUrl = SharePointContext.GetSPHostUrl(HttpContext.Request).AbsoluteUri });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

4a5f7edbe72bfe87442b8a6e5925556534d68d6a
michaju <michal.sobczak@student.wat.edu.pl>
2015-05-29 17:11:03 +0200

    nowa metoda do wyszukiwanai elementow warstwy

--- SWZIM-WEBWeb/Helpers/LayersHelper.cs
+++ SWZIM-WEBWeb/Helpers/LayersHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace SWZIM_WEBWeb.Helpers
 {
@@ -14,6 +15,21 @@ namespace SWZIM_WEBWeb.Helpers
                 var userGroup = db.Users.Find(userId).GroupId;
                 return db.Layers.Where(l => l.Groups.Any(g => g.Id == userGroup)).ToList();
                                
+            }
+        }
+
+        public static List<LayoutElements> GetSearchForUser(int userId, string value)
+        {
+            using (var db = new SWZIM_dbEntities())
+            {
+                var userGroup = db.Users.Find(userId).GroupId;
+                var layers = db.Groups.Find(userGroup).Layers;
+                var result = new List<LayoutElements>();
+                foreach (var l in layers)
+                {
+                    result.AddRange(l.LayoutElements.Where(e => e.Name.Contains(value) || e.Description.Contains(value)));
+                }
+                return result.ToList();
             }
         }
     }

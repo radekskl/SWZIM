e4310232f1f5f119e7b73a4612dae6e790cd3ee4
michaju <michal.sobczak@student.wat.edu.pl>
2015-05-29 17:26:36 +0200

    dopracowywanie widoku wyszukiwania

---
+++ SWZIM-WEBWeb/Models/HelperViewModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SWZIM_WEBWeb.Models
+{
+    public class HelperViewModels
+    {
+        public class SearchViewModel
+        {
+            public Events Event { get; set; }
+            public LayoutElements LayoutElement { get; set; }
+        }
+    }
+}

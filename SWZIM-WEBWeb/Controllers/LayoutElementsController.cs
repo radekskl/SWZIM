243b21da454743f4218d92c19c72b33d0e2d8c35
michaju <michal.sobczak@student.wat.edu.pl>
2015-05-30 23:10:58 +0200

    standardowe poprwaki dla kontrolera

--- SWZIM-WEBWeb/Controllers/LayoutElementsController.cs
+++ SWZIM-WEBWeb/Controllers/LayoutElementsController.cs
@@ -15,6 +15,7 @@ namespace SWZIM_WEBWeb.Controllers
         private SWZIM_dbEntities db = new SWZIM_dbEntities();
 
         // GET: LayoutElements
+        [SharePointContextFilter]
         public ActionResult Index()
         {
             var layoutElements = db.LayoutElements.Include(l => l.LayoutElementTypes).Include(l => l.Users).Include(l => l.Layers);
@@ -22,6 +23,7 @@ namespace SWZIM_WEBWeb.Controllers
         }
 
         // GET: LayoutElements/Details/5
+        [SharePointContextFilter]
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -37,6 +39,7 @@ namespace SWZIM_WEBWeb.Controllers
         }
 
         // GET: LayoutElements/Create
+        [SharePointContextFilter]
         public ActionResult Create()
         {
             ViewBag.LayoutElementTypeId = new SelectList(db.LayoutElementTypes, "Id", "Name");
@@ -50,13 +53,14 @@ namespace SWZIM_WEBWeb.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [SharePointContextFilter]
         public ActionResult Create([Bind(Include = "Id,Name,Description,Longitude,Latitude,LayoutElementTypeId,UserId,LayersId")] LayoutElements layoutElements)
         {
             if (ModelState.IsValid)
             {
                 db.LayoutElements.Add(layoutElements);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", new { SPHostUrl = SharePointContext.GetSPHostUrl(HttpContext.Request).AbsoluteUri });
             }
 
             ViewBag.LayoutElementTypeId = new SelectList(db.LayoutElementTypes, "Id", "Name", layoutElements.LayoutElementTypeId);
@@ -66,6 +70,7 @@ namespace SWZIM_WEBWeb.Controllers
         }
 
         // GET: LayoutElements/Edit/5
+        [SharePointContextFilter]
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -88,13 +93,14 @@ namespace SWZIM_WEBWeb.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [SharePointContextFilter]
         public ActionResult Edit([Bind(Include = "Id,Name,Description,Longitude,Latitude,LayoutElementTypeId,UserId,LayersId")] LayoutElements layoutElements)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(layoutElements).State = EntityState.Modified;
+                db.Entry(layoutElements).State = System.Data.Entity.EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", new { SPHostUrl = SharePointContext.GetSPHostUrl(HttpContext.Request).AbsoluteUri });
             }
             ViewBag.LayoutElementTypeId = new SelectList(db.LayoutElementTypes, "Id", "Name", layoutElements.LayoutElementTypeId);
             ViewBag.UserId = new SelectList(db.Users, "ID", "Email", layoutElements.UserId);
@@ -103,6 +109,7 @@ namespace SWZIM_WEBWeb.Controllers
         }
 
         // GET: LayoutElements/Delete/5
+        [SharePointContextFilter]
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -120,12 +127,13 @@ namespace SWZIM_WEBWeb.Controllers
         // POST: LayoutElements/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [SharePointContextFilter]
         public ActionResult DeleteConfirmed(int id)
         {
             LayoutElements layoutElements = db.LayoutElements.Find(id);
             db.LayoutElements.Remove(layoutElements);
             db.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction("Index", new { SPHostUrl = SharePointContext.GetSPHostUrl(HttpContext.Request).AbsoluteUri });
         }
 
         protected override void Dispose(bool disposing)

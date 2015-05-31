24cf3c34d878bae465cbb813eb574d19507ba920
michaju <michal.sobczak@student.wat.edu.pl>
2015-05-31 17:29:13 +0200

    zmiany w kodzie po zmianie powiazania grupa uzytkownik

--- SWZIM-WEBWeb/Helpers/EventsHelper.cs
+++ SWZIM-WEBWeb/Helpers/EventsHelper.cs
@@ -12,8 +12,18 @@ namespace SWZIM_WEBWeb.Helpers
         {
             using (var db = new SWZIM_dbEntities())
             {
-                var userGroup = db.Users.Find(userId).GroupId;
-                return db.Events.Include(e=> e.EventTypes).Where(l => l.Groups.Any(g => g.Id == userGroup)).OrderByDescending(x=> x.CreatedAt).Take(limit).ToList();
+                var userGroup = db.Users.Find(userId).Groups;
+                var layerGroups = new List<Layers>();
+                foreach (var item in userGroup)
+	            {
+		            layerGroups.AddRange(item.Layers);
+	            }
+                var result = new List<Events>();
+                foreach (var item in layerGroups)
+                {
+                    result.AddRange(item.Events);
+                }
+                return result.OrderByDescending(x=> x.CreatedAt).Take(limit).ToList();
             }
         }
 
@@ -21,8 +31,18 @@ namespace SWZIM_WEBWeb.Helpers
         {
             using (var db = new SWZIM_dbEntities())
             {
-                var userGroup = db.Users.Find(userId).GroupId;
-                return db.Events.Include(e => e.EventTypes).Where(l => l.Groups.Any(g => g.Id == userGroup)).OrderByDescending(x => x.CreatedAt).Where(e => e.Name.Contains(value) || e.Description.Contains(value)).ToList();
+                var userGroup = db.Users.Find(userId).Groups;
+                var layerGroups = new List<Layers>();
+                foreach (var item in userGroup)
+                {
+                    layerGroups.AddRange(item.Layers);
+                }
+                var result = new List<Events>();
+                foreach (var item in layerGroups)
+                {
+                    result.AddRange(item.Events);
+                }
+                return result.OrderByDescending(x => x.CreatedAt).Where(e => e.Name.Contains(value) || e.Description.Contains(value)).ToList();
             }
         }
     }

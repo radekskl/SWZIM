6b96f564fa4c53e3968a312b2f1ed4fa264868a3
michaju <michal.sobczak@student.wat.edu.pl>
2015-06-02 17:01:38 +0200

    nowa funkcja GetXMLStream

--- SWZIM-WEBWeb/Helpers/DataHelper.cs
+++ SWZIM-WEBWeb/Helpers/DataHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,17 @@ namespace SWZIM_WEBWeb.Helpers
 {
     public class DataHelper
     {
+
+        public static Stream GetXMLStream(HttpPostedFileBase file)
+        {
+            using (Stream inputStream = file.InputStream)
+            {
+                MemoryStream memoryStream = inputStream as MemoryStream;
+                memoryStream = new MemoryStream();
+                inputStream.CopyTo(memoryStream);
+                memoryStream.Position = 0;
+                return memoryStream;
+            }
+        }
     }
 }

a94275b3dafa08d3ce14f5ade99f83305ed426c3
michaju <michal.sobczak@student.wat.edu.pl>
2015-06-02 19:25:10 +0200

    nowa metoda getLEtype

---
+++ SWZIM-WEBWeb/Helpers/LayoutElementsHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SWZIM_WEBWeb.Helpers
+{
+    public class LayoutElementsHelper
+    {
+        public static int GetLEType(string input)
+        {
+            int result = 17; // Ogólny
+            using (var db = new SWZIM_dbEntities())
+            {
+                var type = db.LayoutElementTypes.FirstOrDefault(x => x.Name.Equals(input));
+                if (type != null)
+                    result = type.Id;
+            }
+            return result;
+        }
+    }
+}

c97a3cb419b3ec2bf3c3a3efca88f4590cc2cca2
michaju <michal.sobczak@student.wat.edu.pl>
2015-06-02 17:08:50 +0200

    testowa metoda do przegladania xmla

--- SWZIM-WEBWeb/Helpers/DataHelper.cs
+++ SWZIM-WEBWeb/Helpers/DataHelper.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web;
+using System.Xml;
 
 namespace SWZIM_WEBWeb.Helpers
 {
     public class DataHelper
     {
-
         public static Stream GetXMLStream(HttpPostedFileBase file)
         {
             using (Stream inputStream = file.InputStream)
@@ -19,6 +19,41 @@ namespace SWZIM_WEBWeb.Helpers
                 memoryStream.Position = 0;
                 return memoryStream;
             }
+        }
+
+        public static string Test(Stream stream)
+        {
+            string result = "";
+
+            Dictionary<string, string> idToName = new Dictionary<string, string>(); // pozniej za value bedzie obiekt
+            XmlTextReader reader = new XmlTextReader("plik.xmi");
+            while (reader.Read())
+            {
+                if (reader.Name.Equals("packagedElement"))
+                {
+                    var xmiId = reader.GetAttribute("xmi:id");
+                    var xmiName = reader.GetAttribute("name");
+                    idToName.Add(xmiId, xmiName);
+                }
+                if (reader.Prefix.Equals("ProfilCAD"))
+                {
+                    result += reader.LocalName + Environment.NewLine;
+                    if (reader.HasAttributes)
+                    {
+                        while (reader.MoveToNextAttribute())
+                        {
+                            result += String.Format(" {0}={1}", reader.Name, reader.Value) + Environment.NewLine;
+                            if (reader.Name.Equals("base_Class"))
+                                result += String.Format("Nazwa : {0}", idToName[reader.Value]) + Environment.NewLine;
+                        }
+                        reader.MoveToElement();
+                    }
+                }
+
+
+            }
+
+            return result;
         }
     }
 }

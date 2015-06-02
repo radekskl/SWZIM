4ca841f32fdf3f0f6a7ce5bee05ea7c17773ad9e
michaju <michal.sobczak@student.wat.edu.pl>
2015-06-02 16:09:06 +0200

    data controller

---
+++ SWZIM-WEBWeb/Controllers/DataController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SWZIM_WEBWeb.Controllers
+{
+    public class DataController : Controller
+    {
+        // GET: Data
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Import
+        public ActionResult Import()
+        {
+            return View();
+        }
+    }
+}

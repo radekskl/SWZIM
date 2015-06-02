b5b7d38d0b2353c6596a8abedba4f139e2b50309
michaju <michal.sobczak@student.wat.edu.pl>
2015-06-02 17:40:05 +0200

    nowy model do pomocy w interpretowaniu xmi

---
+++ SWZIM-WEBWeb/Models/XMIHelperModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SWZIM_WEBWeb.Models
+{
+    public class XMIHelperModel
+    {
+        public class LatLong
+        {
+            public decimal Latitude { get; set; }
+            public decimal Longitude { get; set; }
+        }
+
+        public class ProfilCADModel
+        {
+            public string Name { get; set; }
+            public string ClassName { get; set; }
+            public Dictionary<string, string> Attributes { get; set; }
+            public LatLong Coordinates { get; set; }
+
+        }
+    }
+}

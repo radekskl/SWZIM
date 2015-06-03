369b8f6044c36f1f2df4bde16dc61a471a7ad31b
michaju <michal.sobczak@student.wat.edu.pl>
2015-06-03 14:01:35 +0200

    tymczasowa wersja wyszukiwarki

--- SWZIM-WEBWeb/Helpers/EventsHelper.cs
+++ SWZIM-WEBWeb/Helpers/EventsHelper.cs
@@ -31,18 +31,19 @@ namespace SWZIM_WEBWeb.Helpers
         {
             using (var db = new SWZIM_dbEntities())
             {
-                var userGroup = db.Users.Find(userId).Groups;
-                var layerGroups = new List<Layers>();
-                foreach (var item in userGroup)
-                {
-                    layerGroups.AddRange(item.Layers);
-                }
-                var result = new List<Events>();
-                foreach (var item in layerGroups)
-                {
-                    result.AddRange(item.Events);
-                }
-                return result.OrderByDescending(x => x.CreatedAt).Where(e => e.Name.Contains(value) || e.Description.Contains(value)).ToList();
+                //var userGroup = db.Users.Find(userId).Groups;
+                //var layerGroups = new List<Layers>();
+                //foreach (var item in userGroup)
+                //{
+                //    layerGroups.AddRange(item.Layers);
+                //}
+                //var result = new List<Events>();
+                //foreach (var item in layerGroups)
+                //{
+                //    result.AddRange(item.Events);
+                //}
+                //return result.OrderByDescending(x => x.CreatedAt).Where(e => e.Name.Contains(value) || e.Description.Contains(value)).ToList();
+                return db.Events.Include(e=> e.EventTypes).OrderByDescending(x => x.CreatedAt).Where(e => e.Name.Contains(value) || e.Description.Contains(value)).ToList();
             }
         }
 
--- SWZIM-WEBWeb/Helpers/LayersHelper.cs
+++ SWZIM-WEBWeb/Helpers/LayersHelper.cs
@@ -27,18 +27,19 @@ namespace SWZIM_WEBWeb.Helpers
         {
             using (var db = new SWZIM_dbEntities())
             {
-                var userGroup = db.Users.Find(userId).Groups;
-                var layerGroups = new List<Layers>();
-                foreach (var item in userGroup)
-                {
-                    layerGroups.AddRange(item.Layers);
-                }
-                var result = new List<LayoutElements>();
-                foreach (var item in layerGroups)
-                {
-                    result.AddRange(item.LayoutElements.Where(e => e.Name.Contains(value) || e.Description.Contains(value)));
-                }
-                return result.ToList();
+                //var userGroup = db.Users.Find(userId).Groups;
+                //var layerGroups = new List<Layers>();
+                //foreach (var item in userGroup)
+                //{
+                //    layerGroups.AddRange(item.Layers);
+                //}
+                //var result = new List<LayoutElements>();
+                //foreach (var item in layerGroups)
+                //{
+                //    result.AddRange(item.LayoutElements.Where(e => e.Name.Contains(value) || e.Description.Contains(value)));
+                //}
+                //return result.ToList();
+                return db.LayoutElements.Where(e => e.Name.Contains(value) || e.Description.Contains(value)).ToList();
             }
         }
 
